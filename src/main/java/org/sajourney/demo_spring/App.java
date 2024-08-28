9bf2e00e5cd77de3d93cc7333c9a792d41011342
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-28 04:30:27 +0300

    Initial Commit

---
+++ src/main/java/org/sajourney/demo_spring/App.java
@@ -0,0 +1,13 @@
+package org.sajourney.demo_spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
