a24e14ccc0c36836a52856b59ad33eb4092074c0
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-29 04:41:08 +0300

    Laptop class implementing interface

--- src/main/java/org/sajourney/demo_spring/Laptop.java
+++ src/main/java/org/sajourney/demo_spring/Laptop.java
@@ -1,6 +1,6 @@
 package org.sajourney.demo_spring;
 
-public class Laptop {
+public class Laptop implements Computer {
     public Laptop(){
         System.out.println("Laptop Constructor");
     }
