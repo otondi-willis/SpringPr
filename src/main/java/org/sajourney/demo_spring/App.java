2892e5d708c2429f59ea3c7c6539bef0a482b697
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-29 05:03:24 +0300

    getting bean by type

--- src/main/java/org/sajourney/demo_spring/App.java
+++ src/main/java/org/sajourney/demo_spring/App.java
@@ -8,7 +8,7 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-       Dev obj = (Dev) context.getBean("dev") ;
+       Dev obj = (Dev) context.getBean(Dev.class) ;
 
 
         obj.build();
