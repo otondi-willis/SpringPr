f265d3e81d1dd0ebe57a859f5fec6a57898863ef
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-29 04:46:28 +0300

    age attribute removed

--- src/main/java/org/sajourney/demo_spring/Dev.java
+++ src/main/java/org/sajourney/demo_spring/Dev.java
@@ -3,20 +3,14 @@ package org.sajourney.demo_spring;
 
 public class Dev {
     private Computer comp;
-    private int age;
+
     public Dev(){
         System.out.println("Dev Constructor");
     }
 
 
 
-    public int getAge() {
-        return age;
-    }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
 
     public Computer getComp() {
         return comp;
@@ -29,7 +23,7 @@ public class Dev {
     public void build(){
         System.out.println("working on awesome project");
 
-       laptop.compile();
+       comp.compile();
 
     }
 }
